bd16c4075d9c36035c2e5105e15852b30a01cf87
joacps02le <jo.acps02le>
2017-06-27 11:27:33 +0200

    create polyglot.jsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { I18n } from 'react-polyglot';
+import poly from './polyglot';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const locale = window.locale || 'en';
+const messages = {
+    "hello_name": "Hello, %{name}.",
+    "num_cars": "%{smart_count} car |||| %{smart_count} cars",
+}
+
+ReactDOM.render(
+    <I18n locale={locale} messages={messages}>
+
+        <poly/>
+    </I18n>,
+
+    document.getElementById('root')
+);
 registerServiceWorker();
